ba34485efac4432032f6accf3e0d63f64c71c379
venkat <venkata-ram@users.noreply.github.com>
2021-03-25 19:00:11 +0530

    add animations

--- app/src/main/java/com/venkataram/mytriviaapp/MainActivity.java
+++ app/src/main/java/com/venkataram/mytriviaapp/MainActivity.java
@@ -1,10 +1,17 @@
 package com.venkataram.mytriviaapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
@@ -56,10 +63,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.true_button:
+
                 checkAnswer(true);
+                updateQuestion();
                 break;
             case R.id.false_button:
+
                 checkAnswer(false);
+                updateQuestion();
                 break;
             case R.id.prev_button:
                 if (currentQuestionIndex == 0)
@@ -82,9 +93,66 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void checkAnswer(boolean userChoice){
         boolean actualAnswer = questionList.get(currentQuestionIndex).isAnswerTrue();
 
-        if(userChoice == actualAnswer)
+        if(userChoice == actualAnswer){
             Toast.makeText(this, "That's correct", Toast.LENGTH_SHORT).show();
-        else
+            fadeAnimation();
+        }
+        else{
             Toast.makeText(this, "That's wrong", Toast.LENGTH_SHORT).show();
+            shakeAnimation();
+        }
+
     }
+    private void shakeAnimation(){
+        Animation shake = AnimationUtils.loadAnimation(MainActivity.this,R.anim.shake_animation);
+        CardView cardView = findViewById(R.id.cardView);
+        cardView.setAnimation(shake);
+
+        shake.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.RED);
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(getColor(R.color.light_sandal));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
+    private void fadeAnimation(){
+        CardView cardView = findViewById(R.id.cardView);
+        AlphaAnimation alphaAnimation = new AlphaAnimation(1.0f,0.0f);
+        alphaAnimation.setDuration(150);
+        alphaAnimation.setRepeatCount(1);
+        alphaAnimation.setRepeatMode(Animation.REVERSE);
+
+        cardView.setAnimation(alphaAnimation);
+
+        alphaAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+                cardView.setCardBackgroundColor(Color.GREEN);
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                cardView.setCardBackgroundColor(getColor(R.color.light_sandal));
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+    }
+
 }
